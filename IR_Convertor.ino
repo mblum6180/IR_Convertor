b9cf7a0e6147a6b748eb70557d79c907f2038d5e
Matthew Blum <45472455+mblum6180@users.noreply.github.com>
2023-04-19 12:47:48 -0400

    Cleaned up

--- IR_Convertor.ino
+++ IR_Convertor.ino
@@ -1,59 +1,52 @@
 #include <IRremote.h>
-int RECV_PIN = 11;
-long NECVolUp = 412553965;
-long NECVolDown = 412586605;
-IRrecv irrecv(RECV_PIN);
-decode_results results;
-IRsend irsend;
+
+const int RECV_PIN = 11;
+const long NEC_VOL_UP = 412553965;
+const long NEC_VOL_DOWN = 412586605;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(RECV_PIN, INPUT);
   
-pinMode(RECV_PIN,INPUT);
-
-irrecv.enableIRIn();
-Serial.println("IR code");
-
-}
-
-void loop() {
-
-  if (irrecv.decode(&results)) {
-    Serial.println(results.value);
-    irrecv.enableIRIn();
-    irrecv.resume(); {
-       if(results.value == NECVolUp ){
-   //delay(50);
-   volUp();  
-      }
-         if(results.value == NECVolDown ){
-     volDown();  
+  // Initialize IR receiver
+  IRrecv irrecv(RECV_PIN);
+  irrecv.enableIRIn();
+  Serial.println("IR code");
+  
+  // Loop for receiving and processing IR signals
+  while (true) {
+    decode_results results;
+    if (irrecv.decode(&results)) {
+      Serial.println(results.value);
+      irrecv.resume();
+      
+      if (results.value == NEC_VOL_UP) {
+        volUp();
+      } else if (results.value == NEC_VOL_DOWN) {
+        volDown();
       }
     }
   }
-
 }
 
+void volUp() {
+  IRsend irsend;
+  for (int i = 0; i < 2; i++) {
+    int khz = 38; // 38kHz carrier frequency for the NEC protocol
+    unsigned int irSignal[] = {1200, 450, 1200, 450, 1250, 450, 1200, 450, 400, 1300, 400, 1250, 400, 1300, 400, 1300, 350, 1300, 1200, 450, 400, 1300, 400};
+    irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
 
-void volUp(){
-      for (int i = 0; i < 2; i++) {
-      int khz = 38; // 38kHz carrier frequency for the NEC protocol
-      unsigned int irSignal[] = {1200,450, 1200,450, 1250,450, 1200,450, 400,1300, 400,1250, 400,1300, 400,1300, 350,1300, 1200,450, 400,1300, 400};
-      irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
-
-      delay(40);
+    delay(40);
   }
-     irrecv.enableIRIn();
 }
 
-void volDown(){
-    for (int i = 0; i < 2; i++) {
-      int khz = 38; // 38kHz carrier frequency for the NEC protocol
-      unsigned int irSignal[] = {1250,450, 1250,450, 1200,450, 1250,450, 350,1300, 400,1300, 350,1300, 400,1300, 1200,450, 400,1250, 400,1300, 400};
-  
-      irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
+void volDown() {
+  IRsend irsend;
+  for (int i = 0; i < 2; i++) {
+    int khz = 38; // 38kHz carrier frequency for the NEC protocol
+    unsigned int irSignal[] = {1250, 450, 1250, 450, 1200, 450, 1250, 450, 350, 1300, 400, 1300, 350, 1300, 400, 1300, 1200, 450, 400, 1250, 400, 1300, 400};
+    irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz);
 
-      delay(40);
-   }
-     irrecv.enableIRIn();
+    delay(40);
+  }
 }
